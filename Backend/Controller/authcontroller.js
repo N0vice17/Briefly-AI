78b94d24f2f4f5f5916bffb07b6e9a39a9e45bd7
debojit <debojitganguly69@gmail.com>
2025-04-15 22:08:30 +0530

    Configuring Backend and Routes

---
+++ Backend/Controller/authcontroller.js
@@ -0,0 +1,21 @@
+const User = require("../models/User")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+exports.register = async (req,res) => {
+    // console.log(req.body);
+    const { username, email, password } = req.body;
+    const hashed = await bcrypt.hash(password,10);
+    const user = new User({ username, email, password: hashed})
+    await user.save()
+    res.status(201).json({msg: "User Registered"});
+};
+
+exports.login = async (req,res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if(!user || !(await bcrypt.compare(password, user.password)))
+        return res.status(401).json({msg : "Invalid Credentials"});
+    const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET || "MAOW")
+    res.json({ token, userId: user._id, username: user.username})
+};
---
+++ Backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const http = require("http")
+const cors = require("cors")
+const { Server } = require("socket.io")
+const connectDB = require("./config/db")
+require("dotenv").config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors: {
+        origin: "*",
+    },
+})
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/messages", require("./routes/message"));
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+    socket.on("join", (userId) => {
+      socket.join(userId);
+    });
+    socket.on("send_message", (data) => {
+      const { from, to, message } = data;
+      io.to(to).emit("receive_message", { from, message, timestamp: Date.now() });
+      const Message = require("./models/Message");
+      const newMsg = new Message({ from, to, message });
+      newMsg.save();
+    });
+  
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+    });
+  });
+  
+  const PORT = process.env.PORT || 3000;
+  server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
