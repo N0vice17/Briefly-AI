8ba07c4f53bbba444229c51f71575aff92368f23
debojit <debojitganguly69@gmail.com>
2025-04-25 13:34:47 +0530

    Changed the UI again

--- Frontend/src/Components/Home/Home.jsx
+++ Frontend/src/Components/Home/Home.jsx
@@ -1,25 +1,10 @@
 import React from "react";
-import { BackgroundBeams } from "@/components/ui/background-beams.jsx";
-import { FlipText } from "@/components/magicui/flip-text";
+import { GridBackground } from "../ui/background-grid";
 
 export default function Home() {
   return (
     <>
-      <div
-        className="h-screen w-screen bg-neutral-950 relative flex flex-col items-center justify-center antialiased brightness-140">
-        <div className="max-w-2xl mx-auto p-4">
-          <h1
-            className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-            <FlipText className="text-4xl font-bold -tracking-widest text-white md:text-7xl md:leading-[5rem]">
-              Briefly AI
-            </FlipText>
-          </h1>
-          <p
-            className="text-neutral-500 max-w-lg mx-auto mt-15 my-2 text-sm text-center relative z-10">
-          </p>
-        </div>
-        <BackgroundBeams />
-      </div>
+      <GridBackground />
     </>
   );
 }
