30e48fc281d9e737d7c641cc052f16c1d3518dcb
debojit <debojitganguly69@gmail.com>
2025-05-01 22:04:59 +0530

    Made the multiple file uploader

--- Frontend/src/Components/Chat/Chat.jsx
+++ Frontend/src/Components/Chat/Chat.jsx
@@ -1,14 +1,60 @@
-import React, { useState } from "react";
-import { SidebarProvider, SidebarTrigger } from "@/Components/ui/sidebar"
-import { AppSidebar } from "@/Components/ui/app-sidebar"
+import React, { useState, useRef } from "react";
+import { Menu, ArrowLeft, ArrowRight, Upload } from 'lucide-react';
 
 export default function Chat() {
+    const [isOpen, setIsOpen] = useState(true);
+    const [files, setFiles] = useState([]);
+    const fileInputRef = useRef(null);
+
+    const handleButtonClick = (e) => {
+        fileInputRef.current.click();
+    };
+
+    const handleFileChange = (e) => {
+        const filename = e.target.files[0].name;
+        if (!files.includes(filename) && files.length < 5) {
+            setFiles((prev) => [...prev, filename]);
+        }
+    };
+
+    const handleRemoveFile = (indexToRemove) => {
+        setFiles((prevItems) => prevItems.filter((_, index) => index !== indexToRemove));
+    };
     return (
         <>
-            <SidebarProvider>
-                <AppSidebar />
-                <SidebarTrigger />
-            </SidebarProvider>
+            <div className="flex h-screen">
+                <div className={`transition-all duration-300 ${isOpen ? 'w-100' : 'w-16'} bg-[#212327] text-white p-4 overflow-hidden`}>
+                    <button onClick={() => setIsOpen(!isOpen)} className="text-white mb-4 focus:outline-none">
+                        {isOpen ? <ArrowLeft size={24} className="cursor-pointer" /> : <ArrowRight size={24} className="cursor-pointer" />}
+                    </button>
+                    <div className={`${isOpen ? 'block' : 'hidden'} space-y-4`}>
+                        <div className="p-4 max-w-md mx-auto space-y-4">
+                            <input type="file" multiple onChange={handleFileChange} ref={fileInputRef} className="hidden" />
+                            <button onClick={handleButtonClick} className="flex flex-row justify-between w-40 bg-white text-black px-4 py-2 rounded cursor-pointer">
+                                Upload Files <Upload/>
+                            </button>
+                            {files.length > 0 && (
+                                <div className="mt-4 space-y-3">
+                                    {files.map((name, index) => (
+                                        <div key={index}
+                                            className="flex items-center justify-between border border-gray-300 rounded-md p-3 bg-white shadow-sm">
+                                            <div className="flex flex-row items-center truncate max-w-full text-sm text-black">
+                                                {name}
+                                            </div>
+                                            <button onClick={() => handleRemoveFile(index)}
+                                                className="text-black ml-4 cursor-pointer">
+                                                ✕
+                                            </button>
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+                <div className="flex-1 p-6 bg-[#292A2D]">
+                </div>
+            </div>
         </>
     )
 }
--- Frontend/src/Components/Home/Home.jsx
+++ Frontend/src/Components/Home/Home.jsx
@@ -2,22 +2,29 @@ import React from "react";
 import { BackgroundBeams } from "../ui/background-beams";
 import { Spotlight } from "../ui/spotlight-new";
 import ollama from "../../assets/ollama.png"
+import { ArrowRight } from "lucide-react"
+import { Link } from "react-router-dom";
 
 
 export default function Home() {
   return (
     <div
       className="h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-        <img src = {ollama} className = "z-20 w-35 h-35"/>
+      <img src={ollama} className="z-20 w-35 h-35" />
       <div className="max-w-2xl mx-auto p-4">
         <Spotlight />
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-white">
           Briefly AI
         </h1>
-        <p className="mt-4 font-bold text-base text-neutral-300 max-w-lg text-center mx-auto">
+        <p className="mt-4 font-bold text-base text-white max-w-lg text-center mx-auto">
           Powered by the GEMMA3 model, our platform allows you to interact with PDF documents through seamless conversations. Easily search, ask questions, and extract key insights from your PDFs, transforming the way you engage with your content
         </p>
       </div>
+      <Link to = "http://localhost:5173/login" className = "z-20">
+        <button className="text-black z-20 bg-white w-25 h-25 mt-7 rounded-xl flex items-center justify-center shadow-white-2xl cursor-pointer">
+          <ArrowRight />
+        </button>
+      </Link>
       <BackgroundBeams />
     </div>
   );
