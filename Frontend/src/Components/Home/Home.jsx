0581f4e18b4c20a492cbf6d664f23b60dde9c778
debojit <debojitganguly69@gmail.com>
2025-05-02 09:24:26 +0530

    Started making login/signup form

--- Frontend/src/Components/Home/Home.jsx
+++ Frontend/src/Components/Home/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div
       className="h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <img src={ollama} className="z-20 w-35 h-35" />
-      <div className="max-w-2xl mx-auto p-4">
+      <div className="z-20 max-w-2xl mx-auto p-4">
         <Spotlight />
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-white">
           Briefly AI
--- Frontend/src/Components/Login and Register/Login.jsx
+++ Frontend/src/Components/Login and Register/Login.jsx
@@ -1,7 +1,28 @@
-export default function Login(){
-    return(
-        <>
-        <h1>Login ko kaun acces kiya bsdk</h1>
-        </>
+import { LoginForm } from "./login-form"
+import { Link } from "react-router-dom"
+
+export default function Login() {
+    return (
+        <div className="grid min-h-svh lg:grid-cols-2">
+            <div className="flex flex-col gap-4 p-6 md:p-10">
+                <div className="flex justify-center gap-2 md:justify-start">
+                    <Link to="http://localhost:5173" className="flex items-center gap-2 font-medium">
+                        Briefly AI
+                    </Link>
+                </div>
+                <div className="flex flex-1 items-center justify-center">
+                    <div className="w-full max-w-xs">
+                        <LoginForm />
+                    </div>
+                </div>
+            </div>
+            <div className="relative hidden bg-muted lg:block">
+                <img
+                    src="/placeholder.svg"
+                    alt="Image"
+                    className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                />
+            </div>
+        </div>
     )
 }
