c15a7839544a9c981962f524f6aa33f4b006d127
debojit <debojitganguly69@gmail.com>
2025-05-09 20:59:06 +0530

    Fixing code for production

--- Frontend/src/Components/Login and Register/signup-form.jsx
+++ Frontend/src/Components/Login and Register/signup-form.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils.js"
 import { Button } from "@/Components/ui/button"
 import { Input } from "@/Components/ui/input"
 import { Label } from "@/Components/ui/label"
