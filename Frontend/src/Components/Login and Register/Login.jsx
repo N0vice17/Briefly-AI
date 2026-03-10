c060aac0262298078840a7e4409e50506332edca
debojit <debojitganguly69@gmail.com>
2026-03-10 11:29:18 +0530

    Fixing Deployment issues

--- Frontend/src/Components/Login and Register/Login.jsx
+++ Frontend/src/Components/Login and Register/Login.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { Zap, ArrowRight, Mail, Lock, Eye, EyeOff } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Button } from "../button";
+import { Input } from "../input";
+import { Label } from "../label";
 import { toast } from "sonner";
 import axios from "axios"
 // import ThemeToggle from "@/components/ThemeToggle";
