80e5c0f1d2d2ba8e53607b57f67194aaceed1eb1
debojit <debojitganguly69@gmail.com>
2026-03-10 11:30:46 +0530

    Fixing production related issues

--- Frontend/src/Components/Login and Register/Login.jsx
+++ Frontend/src/Components/Login and Register/Login.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { Zap, ArrowRight, Mail, Lock, Eye, EyeOff } from "lucide-react";
-import { Button } from "../button";
-import { Input } from "../input";
-import { Label } from "../label";
+import { Button } from "../button.jsx";
+import { Input } from "../input.jsx";
+import { Label } from "../label.jsx";
 import { toast } from "sonner";
 import axios from "axios"
 // import ThemeToggle from "@/components/ThemeToggle";
